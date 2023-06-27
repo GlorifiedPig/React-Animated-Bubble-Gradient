c8a93c4c6ce16b25098c9044f9e3493a12f7d223
Ryan Cherry <ryan@glorifiedstudios.com>
2023-06-27 20:53:50 +0200

    Blur boilerplate

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+
 function Bubble() {
   return (
     <div className="w-64 h-64 bg-blue-500 rounded-full"></div>
@@ -5,16 +6,23 @@ function Bubble() {
 }
 
 function Blur() {
-  
+  return (
+    <div className="bg-blue-50"></div>
+  );
 }
 
 function Background() {
-
+  return (
+    <div className="w-screen h-screen bg-blue-900">
+      <Bubble/>
+      {/* <Blur/> */}
+    </div>
+  );
 }
 
 function App() {
   return (
-    <Bubble/>
+    <Background/>
   );
 }
 
