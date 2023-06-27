98209a914ebf81966486f4b292a6529730315ff1
Ryan Cherry <ryan@glorifiedstudios.com>
2023-06-27 20:48:16 +0200

    BubbleGradient

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+function BubbleGradient() {
+  return (
+    <div className="w-64 h-64 bg-gradient-to-r from-green-400 to-blue-500 rounded-full"></div>
+  )
+}
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <BubbleGradient/>
   )
 }
